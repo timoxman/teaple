4e8e2a553bfb04c3501e385b3dda41ab50e451ed
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-23 16:33:16 +0100

    ONly see messages when signed in

--- spec/features/teaple_features_spec.rb
+++ spec/features/teaple_features_spec.rb
@@ -13,6 +13,7 @@ feature 'teaple' do
 
   context 'user adds new profile' do
     scenario 'user clicks link and adds details' do
+      sign_up(email="test@test.com")
       visit '/'
       click_link "Add your profile"
       fill_in 'Name', with: "Betty"
@@ -24,17 +25,23 @@ feature 'teaple' do
   end
 
   context 'once a teaple has been created you can arrange a meeting with the teaple' do
+
     scenario 'user adds a teaple and can see a link to arrange a meeting' do
+      sign_up(email="test@test.com")
       add_teaple("Martha")
       expect(page).to have_link 'Arrange meeting'
     end
+
     scenario 'user can leave a message on teaple page' do
+      sign_up(email="test@test.com")
       add_teaple
+      visit '/'
       click_link "Arrange meeting"
       fill_in "Message", with: "Please meet me"
       click_button 'Arrange Meeting'
       expect(page).to have_content("Please meet me")
     end
+
   end
 
   context 'viewing teaple' do
@@ -46,5 +53,26 @@ feature 'teaple' do
       expect(current_path).to eq "/teaples/#{betty.id}"
     end
   end
+
+
+  context 'messages' do
+    scenario 'hide private messages from other users' do
+      sign_up(email="test@test.com")
+      add_teaple("Betty123")
+      click_link('Arrange meeting')
+      fill_in 'Message', with: "Heya let us meet"
+      click_button 'Arrange Meeting'
+      expect(page).to have_content "Heya let us meet"
+      @betty = Teaple.find_by(name: "Betty123")
+      sign_out
+      sign_up(email="test1@test.com")
+      visit "/teaples/#{@betty.id}/messages"
+      expect(page).not_to have_content "Heya let us meet"
+
+
+    end
+  end
+
+
 
 end
